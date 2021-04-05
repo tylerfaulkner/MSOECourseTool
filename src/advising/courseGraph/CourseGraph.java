a5b9b63360576428370b62f9a887669fc7e98196
tylerfaulkner <tylersfaulkner@gmail.com>
2021-04-05 18:56:25 -0500

    Added formatting for nodes on screen

--- src/advising/courseGraph/CourseGraph.java
+++ src/advising/courseGraph/CourseGraph.java
@@ -8,7 +8,12 @@ import java.util.Collection;
 import java.util.HashMap;
 
 public class CourseGraph {
-    private static final int NODEDISTANCE = 20;
+    private static final int LINE_OFFSET = 30;
+    private static final int NODES_PER_COL = 25;
+    private static final int COL_OFFSET = 300;
+    private static final int NODE_DISTANCE = 120;
+    private static final int PADDING = 40;
+    private static final int Y_PADDING = 60;
 
     private int highestPreReqCount = 0;
     private ArrayList<CourseNode> nodes = new ArrayList<>();
@@ -20,15 +25,24 @@ public class CourseGraph {
         for(String key : keys){
             addKeytoNodes(key, nodes);
         }
-        addPreReqs();
+        //addPreReqs();
         getHighestPreReqCount();
     }
 
     public void draw(GraphicsContext gc){
+        int offset = 0;
+        int previousNodeY = 0;
         for (int i = 0; i < nodes.size(); i++){
+            int col = i/NODES_PER_COL;
+            if (i%NODES_PER_COL == 0){
+                gc.strokeLine((COL_OFFSET*col)-LINE_OFFSET, 0, (COL_OFFSET*col) - LINE_OFFSET, 3000);
+                offset = 0;
+                previousNodeY = 0;
+            }
             CourseNode node = nodes.get(i);
-            int offset = (highestPreReqCount + 1) - node.getPreReqCount();
-            node.draw(gc, NODEDISTANCE * offset, NODEDISTANCE * i);
+            previousNodeY = offset + previousNodeY + (i > 0 ? NODE_DISTANCE : Y_PADDING);
+            int x = PADDING + (col>=1 ? COL_OFFSET*col : 0);
+            offset = node.draw(gc, x, previousNodeY);
         }
     }
 
--- src/advising/courseGraph/CourseNode.java
+++ src/advising/courseGraph/CourseNode.java
@@ -2,13 +2,17 @@ package advising.courseGraph;
 
 import advising.Course;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Paint;
 
 
 /**
  * A class that represents a single node within the visualized pre-req graph
  */
 public class CourseNode {
-    private static final int NODE_RADIUS = 20;
+    private static final int OUTLINE_RADIUS = 110;
+    private static final int NODE_RADIUS = 100;
+    private static final int Y_SPACING = 120;
+    private static final int X_SPACING = 150;
 
     private Course course;
     private int preReqForCount = 0;
@@ -42,8 +46,28 @@ public class CourseNode {
         return false;
     }
 
-    public void draw(GraphicsContext gc, int x, int y) {
+    public int draw(GraphicsContext gc, int x, int y) {
+        String[] preReqs = course.getPrerequisites().split(" ");
+        int offset = 0;
+        for (int i = 0; i< preReqs.length; ++i){
+            String preReq = preReqs[i];
+            if(preReq.length() > 3) {
+                int yCord = y + (Y_SPACING* i);
+                int ovalX = x - (NODE_RADIUS/3);
+                int ovalY = yCord - (NODE_RADIUS/2);
+                //gc.fillOval(ovalX, ovalY, OUTLINE_RADIUS, OUTLINE_RADIUS);
+                gc.setFill(Paint.valueOf("green"));
+                gc.fillOval(ovalX, ovalY, NODE_RADIUS, NODE_RADIUS);
+                gc.setFill(Paint.valueOf("black"));
+                preReq = preReq.replaceAll("\\|", " or\n");
+                gc.fillText(preReq, x, yCord, 50);
+                if (i >= 1) {
+                    offset += Y_SPACING;
+                }
+            }
+        }
         //gc.fillOval(x, y, NODE_RADIUS, NODE_RADIUS);
-        gc.fillText(course.getName(), x, y);
+        gc.fillText(course.getName(), x + X_SPACING, y + (offset/2));
+        return offset;
     }
 }
