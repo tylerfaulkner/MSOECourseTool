6caf1e42f521a3a96fac992261d345b25abe0c4f
tylerfaulkner <tylersfaulkner@gmail.com>
2021-04-06 12:13:14 -0500

    Added lines drawn between course and pre-requisites

--- src/advising/courseGraph/CourseNode.java
+++ src/advising/courseGraph/CourseNode.java
@@ -4,6 +4,8 @@ import advising.Course;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Paint;
 
+import java.util.ArrayList;
+
 
 /**
  * A class that represents a single node within the visualized pre-req graph
@@ -48,6 +50,7 @@ public class CourseNode {
 
     public int draw(GraphicsContext gc, int x, int y) {
         String[] preReqs = course.getPrerequisites().split(" ");
+        ArrayList<int[]> cords = new ArrayList<>();
         int offset = 0;
         for (int i = 0; i< preReqs.length; ++i){
             String preReq = preReqs[i];
@@ -61,13 +64,27 @@ public class CourseNode {
                 gc.setFill(Paint.valueOf("black"));
                 preReq = preReq.replaceAll("\\|", " or\n");
                 gc.fillText(preReq, x, yCord, 50);
+                cords.add(new int[]{x, yCord});
                 if (i >= 1) {
                     offset += Y_SPACING;
                 }
             }
         }
-        //gc.fillOval(x, y, NODE_RADIUS, NODE_RADIUS);
-        gc.fillText(course.getName(), x + X_SPACING, y + (offset/2));
+        int xCord = x + X_SPACING;
+        int yCord = y + (offset/2);
+        System.out.println(cords.size());
+        if (cords.size() == 1){
+            yCord = cords.get(0)[1];
+        }
+        for (int[] cord : cords){
+            int xPreReq = cord[0];
+            int yPreReq = cord[1];
+            gc.strokeLine(xPreReq+(NODE_RADIUS/1.5), yPreReq, xCord, yCord);
+        }
+        gc.setFill(Paint.valueOf("blue"));
+        gc.fillOval(xCord - 25, yCord - (NODE_RADIUS/2), NODE_RADIUS, NODE_RADIUS);
+        gc.setFill(Paint.valueOf("black"));
+        gc.fillText(course.getName(), xCord, yCord);
         return offset;
     }
 }
